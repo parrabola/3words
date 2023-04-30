52018b4d1d666915845c2c0f696a0dded4a2b825
parrabola <maxim.keip@gmail.com>
2023-04-30 14:18:36 +0300

    remove token

--- config.py
+++ config.py
@@ -3,6 +3,6 @@ vote_time = 30
 winner_time = 15
 min_votes_for_complete_quest = 1
 min_answers_for_start_voting = 2
-token = "902363508:AAF-FPzVC_wNLjKIFRshL_vtYVMZrxZC7qs"
+token = "your_telegram_token_here"
 is_anonimous_polls = False
 is_allows_multiple_answers = False
